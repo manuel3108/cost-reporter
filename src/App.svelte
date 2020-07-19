94d3f9d7ad403b91476dbb3cb832008b846b9c2f
Manuel <30698007+manuel3108@users.noreply.github.com>
2020-07-19 16:58:20 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let application;
+
+	import Routes from './Routes.svelte'
+	import Navbar from './components/Navbar.svelte';
+</script>
+
+<style global lang="scss">
+	@import "main.scss";
+</style>
+
+<Navbar bind:application></Navbar>
+<main>
+	<Routes></Routes>
+</main>
+
